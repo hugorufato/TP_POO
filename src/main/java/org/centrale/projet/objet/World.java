0138d6c019ac95c40f09bbfefa955fcefa874e90
ADM <ADM@130.66.209.4>
2023-10-15 10:45:39 +0200

    15/10

--- src/main/java/org/centrale/projet/objet/World.java
+++ src/main/java/org/centrale/projet/objet/World.java
@@ -72,6 +72,11 @@ public class World {
      *
      */
     private ArrayList<Creature> creatures;
+    
+    /**List avec des objets
+     *
+     */
+    private ArrayList<Objet> objets;
 
     /**List avec des personnages
      *
@@ -114,94 +119,37 @@ public class World {
         loups = new ArrayList();
         creatures = new ArrayList();
         personnages = new LinkedList();
+        objets = new ArrayList();
         countvie = 0;
         nombreProt = 100;
         grille = new ElemJeu[50][50];
+        
 
     }
 
-    /**Méthode pour créer des éléments du jeu et faire des tests
-     *
-     */
-    public void creerMondeAlea() {
 
-        Random aleat = new Random();
-        int nombre = aleat.nextInt(100);
-        for (int i = 0; i < nombre; i++) {
-            archers.add(new Archer());
-        }
-        nombre = aleat.nextInt(100);
-        for (int i = 0; i < nombre; i++) {
-            paysans.add(new Paysan());
-        }
-        nombre = aleat.nextInt(100);
-        for (int i = 0; i < nombre; i++) {
-            lapins.add(new Lapin());
-        }
-        nombre = aleat.nextInt(100);
-        for (int i = 0; i < nombre; i++) {
-            guerriers.add(new Guerrier());
-        }
-        nombre = aleat.nextInt(100);
-        for (int i = 0; i < nombre; i++) {
-            loups.add(new Loup());
-        }
-
-        ////////////////////////////////////////
-        for (int i = 0; i < archers.size(); i++) {
-            creatures.add(archers.get(i));
-        }
-        for (int i = 0; i < paysans.size(); i++) {
-            creatures.add(paysans.get(i));
-        }
-        for (int i = 0; i < guerriers.size(); i++) {
-            creatures.add(guerriers.get(i));
-        }
-
-        for (int i = 0; i < creatures.size(); i++) {
-            countvie = creatures.get(i).getptVie() + countvie;
-        }
-        ///////////////////////////////////////////
-        //TEST TEMPS
-
-        System.out.println("Mesure de temps avec " + nombreProt + " personnages");
-        for (int i = 0; i < nombreProt / 4; i++) {
-            personnages.add(new Archer());
-            personnages.add(new Paysan());
-            personnages.add(new Guerrier());
-            personnages.add(new Archer());
-        }
-        countvie = 0;
-        long debut = System.nanoTime();
-        for (int i = 0; i < personnages.size(); i++) {
-            countvie = personnages.get(i).getptVie() + countvie;
-        }
-        long fin = System.nanoTime();
-        System.out.println("temps boucle basée sur la taille: " + (fin - debut) + "ns");
-        debut = System.nanoTime();
-        for (Personnage p : personnages) {
-            countvie = p.getptVie() + countvie;
-        }
-        fin = System.nanoTime();
-        System.out.println("temps boucle basée sur les itérateurs: " + (fin - debut) + "ns");
-
-    }
-    ///////////////////////////////////////////////////////////////
     
     public void CreerMondeAlea(int taille){
-    
+
+    if(taille<=2){
+    System.out.println("ERROR - TAILLE DU MONDE DOIT ETRE >= 2");
+    }
+    else{
+            
     this.grille = new ElemJeu[taille][taille];
     
 //Creation
     Random aleat = new Random();
         
-        int nA = aleat.nextInt(taille*taille);
-        int nP = aleat.nextInt(taille*taille);
-        int nL = aleat.nextInt(taille*taille);
-        int nG = aleat.nextInt(taille*taille);
-        int nLo = aleat.nextInt(taille*taille);
+        int nA = aleat.nextInt(taille);
+        int nP = aleat.nextInt(taille);
+        int nL = aleat.nextInt(taille);
+        int nG = aleat.nextInt(taille);
+        int nLo = aleat.nextInt(taille);
+        int npotion = aleat.nextInt(2,taille);
+        int nepee = aleat.nextInt(2,taille);
         
-        while((nA+nP+nL+nG+nLo)>(taille*taille)){
+        while((nA+nP+nL+nG+nLo)>(taille*taille-5)){
         nA = aleat.nextInt(taille*taille);
         nP = aleat.nextInt(taille*taille);
         nL = aleat.nextInt(taille*taille);
@@ -212,24 +160,50 @@ public class World {
         
                 
         for (int i = 0; i < nA; i++) {
-            archers.add(new Archer());
+            this.archers.add(new Archer());
         }
         
         for (int i = 0; i < nP; i++) {
-            paysans.add(new Paysan());
+            this.paysans.add(new Paysan());
         }
         
         for (int i = 0; i < nL; i++) {
-            lapins.add(new Lapin());
+            this.lapins.add(new Lapin());
         }
         
         for (int i = 0; i < nG; i++) {
-            guerriers.add(new Guerrier());
+            this.guerriers.add(new Guerrier());
         }
         
         for (int i = 0; i < nLo; i++) {
-            loups.add(new Loup());
+            this.loups.add(new Loup());
         }
+        
+        for (int i = 0; i < npotion; i++) {
+            this.objets.add(new PotionSoin());
+        }
+        
+        for (int i = 0; i < nepee; i++) {
+            this.objets.add(new Epee());
+        }
+        
+        /////// Ajouter en creature
+        for (int i = 0; i < this.archers.size(); i++) {
+            this.creatures.add(this.archers.get(i));
+        }
+        for (int i = 0; i < this.paysans.size(); i++) {
+            this.creatures.add(this.paysans.get(i));
+        }
+        for (int i = 0; i < this.guerriers.size(); i++) {
+            this.creatures.add(this.guerriers.get(i));
+        }
+        for (int i = 0; i < this.lapins.size(); i++) {
+            this.creatures.add(this.lapins.get(i));
+        }
+        for (int i = 0; i < this.loups.size(); i++) {
+            this.creatures.add(this.loups.get(i));
+        }
+        
     
     //Placement
         //Archers
@@ -237,9 +211,9 @@ public class World {
          while(k <nA){
             int x = aleat.nextInt(taille);
             int y = aleat.nextInt(taille);
-            if(grille[x][y]==null){
-            archers.get(k).getpos().setPosition(x,y);
-            grille[x][y]=archers.get(k);
+            if(this.grille[x][y]==null){
+            this.archers.get(k).getpos().setPosition(x,y);
+            this.grille[x][y]=this.archers.get(k);
             k++;
             }
             
@@ -249,9 +223,9 @@ public class World {
          while(k <nP){
             int x = aleat.nextInt(taille);
             int y = aleat.nextInt(taille);
-            if(grille[x][y]==null){
-            paysans.get(k).getpos().setPosition(x,y);
-            grille[x][y]=paysans.get(k);
+            if(this.grille[x][y]==null){
+            this.paysans.get(k).getpos().setPosition(x,y);
+            this.grille[x][y]=this.paysans.get(k);
             k++;
             }
             
@@ -261,9 +235,9 @@ public class World {
          while(k <nL){
             int x = aleat.nextInt(taille);
             int y = aleat.nextInt(taille);
-            if(grille[x][y]==null){
-            lapins.get(k).getpos().setPosition(x,y);
-            grille[x][y]=lapins.get(k);
+            if(this.grille[x][y]==null){
+            this.lapins.get(k).getpos().setPosition(x,y);
+            this.grille[x][y]=this.lapins.get(k);
             k++;
             }
             
@@ -273,9 +247,9 @@ public class World {
          while(k <nG){
             int x = aleat.nextInt(taille);
             int y = aleat.nextInt(taille);
-            if(grille[x][y]==null){
-            guerriers.get(k).getpos().setPosition(x,y);
-            grille[x][y]=guerriers.get(k);
+            if(this.grille[x][y]==null){
+            this.guerriers.get(k).getpos().setPosition(x,y);
+            this.grille[x][y]=this.guerriers.get(k);
             k++;
             }
             
@@ -285,47 +259,177 @@ public class World {
          while(k <nLo){
             int x = aleat.nextInt(taille);
             int y = aleat.nextInt(taille);
-            if(grille[x][y]==null){
-            loups.get(k).getpos().setPosition(x,y);
-            grille[x][y]=loups.get(k);
+            if(this.grille[x][y]==null){
+            this.loups.get(k).getpos().setPosition(x,y);
+            this.grille[x][y]=this.loups.get(k);
             k++;
             }
             
         }
-        System.out.println(nA+" "+nP+" "+nL+" "+nG+" "+nLo);
-        System.out.println(taille*taille);
-    
+         
+        // Objets
+        k=0;
+         while(k <(npotion+nepee)){
+            int x = aleat.nextInt(taille);
+            int y = aleat.nextInt(taille);
+            if(this.grille[x][y]==null){
+            this.objets.get(k).getpos().setPosition(x,y);
+            this.grille[x][y]=this.objets.get(k);
+            k++;
+            }
+            
+        }
+        
+//        System.out.println(nA+" "+nP+" "+nL+" "+nG+" "+nLo);
+//        System.out.println(this.creatures.size());
     
     }
+    }
 
+    public void modGrille(int x, int y, ElemJeu element){
+    this.grille[x][y] = element;
+    }
+    
+    public ElemJeu verifierGrille(int x,int y){
+    return this.grille[x][y];
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
     /**Tour de jeu
      *
      */
-    public void TourDeJeu() {
-        
+ public void TourDeJeu() {
+    
      
+     
+     
+     
+     
+    // Deplacement
+    for (int i = 0; i < this.creatures.size(); i++) {
+        int k = 0;
+        Point2D p = new Point2D();
+        p.setPosition(this.creatures.get(i).getpos().getX(), this.creatures.get(i).getpos().getY());
+        this.creatures.get(i).deplace();
 
+        while (k < 8) {
+            // Verifica se a criatura está fora dos limites da grade.
+                this.creatures.get(i).deplace();
+                while (this.creatures.get(i).getpos().getX() < 0 || this.creatures.get(i).getpos().getX() > this.grille.length-1  ||
+                        this.creatures.get(i).getpos().getY() < 0 || this.creatures.get(i).getpos().getY() > this.grille.length-1 ) {
+
+                    this.creatures.get(i).getpos().setPosition(p.getX(), p.getY());
+                    this.creatures.get(i).deplace();
+                }     
+
+                        
+                        
+                if (this.grille[this.creatures.get(i).getpos().getX()][this.creatures.get(i).getpos().getY()] != null) {
+                    if(this.grille[this.creatures.get(i).getpos().getX()][this.creatures.get(i).getpos().getY()] instanceof Objet o
+                            && this.creatures.get(i) instanceof Personnage f) {
+                            f.useitem(o);
+                            this.grille[p.getX()][p.getY()] = null;
+                            this.grille[this.creatures.get(i).getpos().getX()][this.creatures.get(i).getpos().getY()] = this.creatures.get(i);
+                            k=8;
+                    }
+                    else{
+                    this.creatures.get(i).getpos().setPosition(p.getX(), p.getY());}
+                    
+                } else {
+                    this.grille[p.getX()][p.getY()] = null;
+                    this.grille[this.creatures.get(i).getpos().getX()][this.creatures.get(i).getpos().getY()] = this.creatures.get(i);
+//                    System.out.println("coloquei o: ["+p.getX()+";"+p.getY()+"] no: ["+this.creatures.get(i).getpos().getX()+";"+this.creatures.get(i).getpos().getY()+"] na iteração "+i);
+                    k=8;
+                    }
+
+            k++;
+        }
+    }  
+    
+    //COMBATTRE
+    
+    
+    
+}
+        
+    /**Placer un personnage dans le monde
+     *
+     */
+ 
+    public void Placer(Personnage p){
+        Random aleat = new Random();
+        int k=0;
+        while(k <1){
+            int x = aleat.nextInt(this.grille.length);
+            int y = aleat.nextInt(this.grille.length);
+            if(this.grille[x][y]==null){
+            p.getpos().setPosition(x,y);
+            this.grille[x][y]=p;
+            k++;
+            }
     }
+    }
+        
 
     /**Afficher le monde
      *
      */
     public void afficheWorld() {
-     for (int i = 0; i < this.grille.length; i++) {
-            for (int j = 0; j < this.grille[i].length; j++) {
-                if (this.grille[i][j] == null) {
-                    System.out.print(0+ " ");
-                } else {
-                    System.out.print(this.grille[i][j] + " ");
-                }
+    int numRows = this.grille[0].length;
+    int numCols = this.grille.length;
+
+    // Encontre o comprimento máximo do nome na matriz para definir o tamanho da célula
+    int maxNomeLength = 1; // Tamanho mínimo 1
+
+    for (int row = 0; row < numRows; row++) {
+        for (int col = 0; col < numCols; col++) {
+            if (this.grille[row][col] != null) {
+                int nomeLength = this.grille[row][col].toString().length();
+                maxNomeLength = Math.max(maxNomeLength, nomeLength);
             }
-            System.out.println(); 
         }
     }
+    int cellSize = maxNomeLength + 2; // Tamanho da célula
+
+    // Imprime a parte superior da grade
+    for (int row = numRows - 1; row >= 0; row--) {
+        System.out.printf("%-" + maxNomeLength + "d  ", row); // Número da linha (eixo Y) com espaços para alinhar
+        for (int col = 0; col < numCols; col++) {
+            if (this.grille[row][col] == null) {
+                System.out.printf("%-" + cellSize + "s", "0"); // Imprime "0" nas posições nulas
+            } else {
+                String nome = this.grille[row][col].toString();
+                System.out.printf("%-" + cellSize + "s", nome); // Nome no quadrado com espaços para alinhar
+            }
+        }
+        System.out.println();
+    }
+    System.out.println();
+    System.out.printf("%-" + cellSize + "s", "");
+    // Imprime o marcador "Y" no canto superior esquerdo, após o último número da matriz
+    for (int col = 0; col < numCols; col++) {
+        System.out.printf("%-" + cellSize + "s", col);
+    }
+    
+}
+
+
+
+
+
+
+
+
+}
     
         
-        
-        
-    }
+    
 
 
